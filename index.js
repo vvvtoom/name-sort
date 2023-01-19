5e27f3f57637fbacdf9cb6efdc870df9dd7f42d2
vvvtoom@gmail.com <vvvtoom@gmail.com>
2023-01-19 13:59:36 +0900

    stylized

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var name_sort = () => {
-    const nameList = document.getElementById("nameTextarea").value.replace(/ +/g, " ").split(' ');
+    const nameList = document.getElementById("nameTextarea").value.replace(/ +/g, " ").replace(/(?:\r\n|\r|\n)/g, " ").split(' ');
     document.getElementById("nameTextarea").value = nameList.sort().join(' ');
 }
